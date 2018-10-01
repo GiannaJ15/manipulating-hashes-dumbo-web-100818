b80bde7fe626191e01fceb34290d6836b1bc7d9b
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-10-01 23:28:04 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,7 +18,12 @@ def first_challenge
 contact.each do |person, data|
   data.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
-      value.delete_if 
+      value.delete_if |flavor|
+      flavor == "strawberry"
+      end
+    end
+  end 
+    
   #remember to return your newly altered contacts hash!
   contacts
 end

61830f5f75d5f71d020ef0f068ee159d217bfd51
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-10-01 23:31:52 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,10 +15,15 @@ def first_challenge
 
   #your code here
 
-contacts["Freddy mercury"][:favorite_icecream_flavors].delete_if do |flavor|
-  flavor == "strawberry"
+contacts.each do |person, data|
+  data.each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete_if do |flavor|
+      flavor == "strawberry"
+      end
+    end
+  end 
 end
-    
   #remember to return your newly altered contacts hash!
   contacts
 end
